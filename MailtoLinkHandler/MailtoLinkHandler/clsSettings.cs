f506982ac65361256b32d5a19e47bf288892f4dc
Katy Nicholson <katy@katynicholson.uk>
2021-01-26 22:25:48 +0000

    Update

--- MailtoLinkHandler/MailtoLinkHandler/clsSettings.cs
+++ MailtoLinkHandler/MailtoLinkHandler/clsSettings.cs
@@ -57,7 +57,7 @@ namespace MailtoHandler
             {
                 configFilePath = Path.Combine(FilePath, FileName);
             }
-                
+
             else
             {
                 configFilePath = Path.Combine(Environment.CurrentDirectory, FileName);
@@ -113,11 +113,11 @@ namespace MailtoHandler
         {
             return _get(Setting, Encrypted, Default);
         }
-        protected virtual string _get(string Setting, bool Encrypted = false, string Default = null) {
+        protected virtual string _get(string Setting, bool Encrypted = false, string Default = null)
+        {
             DataRow[] dResult = dsSettings.Tables["Settings"].Select("Setting='" + Setting + "'");
             if (dResult.Count() == 1)
             {
-                if (Encrypted) return Decrypt(dResult[0]["Value"].ToString(), true);
                 return dResult[0]["Value"].ToString();
             }
             if (Default != null) return Default;
@@ -136,27 +136,16 @@ namespace MailtoHandler
             if (dResult.Count() == 1)
             {
                 DataRow dRow = dResult[0];
-                if (Encrypted)
-                {
-                    dRow["Value"] = Encrypt(Value, true);
-                }
-                else
-                {
-                    dRow["Value"] = Value;
-                }
+                dRow["Value"] = Value;
                 bolChanged = true;
                 return;
-            } else if(dResult.Count() == 0) {
+            }
+            else if (dResult.Count() == 0)
+            {
                 DataRow dRow = dsSettings.Tables["Settings"].NewRow();
                 dRow["Setting"] = Setting;
-                if (Encrypted)
-                {
-                    dRow["Value"] = Encrypt(Value, true);
-                }
-                else
-                {
-                    dRow["Value"] = Value;
-                }
+                dRow["Value"] = Value;
+
                 dsSettings.Tables["Settings"].Rows.Add(dRow);
                 bolChanged = true;
                 return;
@@ -168,9 +157,10 @@ namespace MailtoHandler
         /// Saves settings to XML file
         /// </summary>
         /// <param name="fileName">(Optional) Path and filename, will use original filename if not specified</param>
-        public void saveXML(string fileName = null) {
+        public void saveXML(string fileName = null)
+        {
             bolChanged = false;
-            
+
             if (fileName != null)
             {
                 Directory.CreateDirectory(Path.GetDirectoryName(fileName));
@@ -181,106 +171,7 @@ namespace MailtoHandler
             dsSettings.WriteXml(configFilePath);
         }
 
-        /// <summary>
-        /// Encrypts data.
-        /// </summary>
-        /// <param name="toEncrypt">The string to encrypt</param>
-        /// <param name="useHashing">Whether to use hashing</param>
-        /// <returns>Encrypted string</returns>
-        public static string Encrypt(string toEncrypt, bool useHashing)
-        {
-            byte[] keyArray;
-            byte[] toEncryptArray = UTF8Encoding.UTF8.GetBytes(toEncrypt);
-
-            
-            string key = "AK$pa$$w0rdS3Cr£t1744";
-            //System.Windows.Forms.MessageBox.Show(key);
-            //If hashing use get hashcode regards to your key
-            if (useHashing)
-            {
-                MD5CryptoServiceProvider hashmd5 = new MD5CryptoServiceProvider();
-                keyArray = hashmd5.ComputeHash(UTF8Encoding.UTF8.GetBytes(key));
-                //Always release the resources and flush data
-                // of the Cryptographic service provide. Best Practice
-
-                hashmd5.Clear();
-            }
-            else
-                keyArray = UTF8Encoding.UTF8.GetBytes(key);
-
-            TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
-            //set the secret key for the tripleDES algorithm
-            tdes.Key = keyArray;
-            //mode of operation. there are other 4 modes.
-            //We choose ECB(Electronic code Book)
-            tdes.Mode = CipherMode.ECB;
-            //padding mode(if any extra byte added)
-
-            tdes.Padding = PaddingMode.PKCS7;
-
-            ICryptoTransform cTransform = tdes.CreateEncryptor();
-            //transform the specified region of bytes array to resultArray
-            byte[] resultArray =
-              cTransform.TransformFinalBlock(toEncryptArray, 0,
-              toEncryptArray.Length);
-            //Release resources held by TripleDes Encryptor
-            tdes.Clear();
-            //Return the encrypted data into unreadable string format
-            return Convert.ToBase64String(resultArray, 0, resultArray.Length);
-        }
-        private static string Decrypt(string cipherString, bool useHashing)
-        {
-            byte[] keyArray;
-            //get the byte code of the string
-
-            byte[] toEncryptArray = Convert.FromBase64String(cipherString);
-
-            string key = "AK$pa$$w0rdS3Cr£t1744";
-
-            if (useHashing)
-            {
-                //if hashing was used get the hash code with regards to your key
-                MD5CryptoServiceProvider hashmd5 = new MD5CryptoServiceProvider();
-                keyArray = hashmd5.ComputeHash(UTF8Encoding.UTF8.GetBytes(key));
-                //release any resource held by the MD5CryptoServiceProvider
-
-                hashmd5.Clear();
-            }
-            else
-            {
-                //if hashing was not implemented get the byte code of the key
-                keyArray = UTF8Encoding.UTF8.GetBytes(key);
-            }
-
-            TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
-            //set the secret key for the tripleDES algorithm
-            tdes.Key = keyArray;
-            //mode of operation. there are other 4 modes. 
-            //We choose ECB(Electronic code Book)
-
-            tdes.Mode = CipherMode.ECB;
-            //padding mode(if any extra byte added)
-            tdes.Padding = PaddingMode.PKCS7;
-
-            ICryptoTransform cTransform = tdes.CreateDecryptor();
-            try
-            {
-                byte[] resultArray = cTransform.TransformFinalBlock(
-                                     toEncryptArray, 0, toEncryptArray.Length);
-
-                  //Release resources held by TripleDes Encryptor                
-                tdes.Clear();
-               //return the Clear decrypted TEXT
-                 return UTF8Encoding.UTF8.GetString(resultArray);
-            }
-            catch
-            {
-                tdes.Clear();
-                return "";
-            }
-        }
     }
-
     /// <summary>
     /// Class to hold a setting's name and value.
     /// </summary>
@@ -296,35 +187,4 @@ namespace MailtoHandler
         }
     }
 
-    public class SerializePrinterSetting
-    {
-        public string SettingToString(PrinterSettings settings)
-        {
-            if (settings == null)
-                return null;
-
-            var bf = new BinaryFormatter();
-            using (var ms = new MemoryStream())
-            {
-                bf.Serialize(ms, settings);
-                return Convert.ToBase64String(ms.ToArray());
-            }
-        }
-
-        public PrinterSettings SettingFromString(string base64)
-        {
-            try
-            {
-                BinaryFormatter bf = new BinaryFormatter();
-                using (var ms = new MemoryStream(Convert.FromBase64String(base64)))
-                {
-                    return (PrinterSettings)bf.Deserialize(ms);
-                }
-            }
-            catch (Exception)
-            {
-                return new PrinterSettings();
-            }
-        }
-    }
 }
