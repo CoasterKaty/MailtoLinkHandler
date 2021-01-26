735da33eee9accf816ef653b7bbf5b656fac6418
Katy Nicholson <katy@katynicholson.uk>
2021-01-26 22:08:56 +0000

    Add project files.

---
+++ MailtoHandler/Program.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using Microsoft.Win32;
+using System.IO;
+using System.Diagnostics;
+using Outlook = Microsoft.Office.Interop.Outlook;
+
+namespace MailtoHandler
+{
+
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            try
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                if (args.Length > 0)
+                {
+
+                    string currentUserDomain = Regex.Split(System.DirectoryServices.AccountManagement.UserPrincipal.Current.UserPrincipalName, "@")[1];
+                    string arg = String.Join(" ", args);
+
+                    string mailTo = Regex.Split(((string)arg), "mailto:")[1];
+                    string mailSubject = "";
+                    string mailBody = "";
+
+                    if (mailTo.Contains('?'))
+                    {
+                        mailTo = mailTo.Replace('?', '&');
+                    }
+                    if (mailTo.Contains('&'))
+                    {
+                        string[] mailArgs = mailTo.Split('&');
+                        mailTo = mailArgs[0];
+                        foreach (string mailArg in mailArgs)
+                        {
+                            if (mailArg.Contains('='))
+                            {
+                                string[] item = mailArg.Split(new Char[] { '=' }, 2);
+                                switch (item[0].ToLower())
+                                {
+                                    case "subject":
+                                        mailSubject = item[1];
+                                        break;
+                                    case "body":
+                                        mailBody = item[1];
+                                        break;
+                                }
+                            }
+                        }
+                    }
+                    
+
+                    
+                    Settings kSettings = new Settings(true, "mailtohandler.xml", Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData));
+                    if (kSettings.Get("OpenInOutlook", "notconfig") == "notconfig" || kSettings.Get("DontAsk", "no") == "no")
+                    {
+                        frmPick fPick = new frmPick();
+
+                        Settings kAppSettings = new Settings(true);
+                        fPick.rad365.Text = kAppSettings.Get("365_Radio", "Outlook 365");
+                        fPick.rad2016.Text = kAppSettings.Get("Client_Radio", "Outlook Client");
+                        fPick.label365.Text = kAppSettings.Get("365_Text", "For most people, or if you are not sure, select this option");
+                        fPick.labelClient.Text = kAppSettings.Get("Client_Text", "For some people in fixed offices or with multiple mailboxes");
+                        fPick.labelTitle.Text = kAppSettings.Get("Main_Text", "Please select how you wish to open mail links");
+                        fPick.chkDontAsk.Text = kAppSettings.Get("Remember", "Don't ask again");
+                        fPick.Text = kAppSettings.Get("Title", "Open Mail Link");
+
+                        switch (kSettings.Get("OpenInOutlook", "notconfig"))
+                        {
+                            case "yes":
+                                fPick.rad365.Checked = false;
+                                fPick.rad2016.Checked = true;
+                                break;
+                            case "no":
+                                fPick.rad365.Checked = true;
+                                fPick.rad2016.Checked = false;
+                                break;
+
+                        }
+                        fPick.chkDontAsk.Checked = (kSettings.Get("DontAsk", "yes") == "yes");
+                        DialogResult dRes = fPick.ShowDialog();
+                        switch (dRes)
+                        {
+                            case DialogResult.Yes:
+                                kSettings.Set("OpenInOutlook", "no");
+                                kSettings.Set("DontAsk", "yes");
+                                break;
+                            case DialogResult.OK:
+                                kSettings.Set("OpenInOutlook", "no");
+                                kSettings.Set("DontAsk", "no");
+                                break;
+                            case DialogResult.No:
+                                kSettings.Set("OpenInOutlook", "yes");
+                                kSettings.Set("DontAsk", "yes");
+                                break;
+                            case DialogResult.Cancel:
+                                kSettings.Set("OpenInOutlook", "yes");
+                                kSettings.Set("DontAsk", "no");
+                                break;
+                        }
+                    }
+                    if (kSettings.Get("OpenInOutlook", "notconfig") == "no")
+                    {
+                        string mailtoURL = "https://outlook.office.com/owa/?realm=" + currentUserDomain + "&path=/mail/action/compose&to=" + mailTo + "&subject=" + mailSubject + "&body=" + mailBody;
+                        OpenInDefaultBrowser(mailtoURL);
+                    }
+                    else
+                    {
+                        SendOutlookMail(mailTo, mailSubject, mailBody);
+                    }
+                    kSettings.saveXML();
+
+                }
+            }
+            catch
+            {
+            }
+            
+        }
+
+        public static Boolean SendOutlookMail(string recipient, string subject, string body)
+        {
+            try
+            {
+                Outlook.Application outlookApp = new Outlook.Application();
+                Outlook._MailItem oMailItem = (Outlook._MailItem)outlookApp.CreateItem(Outlook.OlItemType.olMailItem);
+                Outlook.Inspector oInspector = oMailItem.GetInspector;
+
+                Outlook.Recipients oRecips = (Outlook.Recipients)oMailItem.Recipients;
+                Outlook.Recipient oRecip = (Outlook.Recipient)oRecips.Add(recipient);
+                oRecip.Resolve();
+
+                if (subject != "")
+                {
+                    oMailItem.Subject = subject;
+                }
+                if (body != "")
+                {
+                    oMailItem.Body = body;
+                }
+                oMailItem.Display(true);
+                return true;
+            }
+            catch (Exception objEx)
+            {
+                MessageBox.Show(objEx.ToString());
+                return false;
+            }
+        }
+
+
+
+        /// <summary>
+        ///     Opens a local file or url in the default web browser.
+        ///     Can be used both for opening urls, or html readme docs.
+        ///     Credit: https://stackoverflow.com/questions/13621467/how-to-find-default-web-browser-using-c
+        /// </summary>
+        /// <param name="pathOrUrl">Path of the local file or url</param>
+        /// <returns>False if the default browser could not be opened.</returns>
+        public static Boolean OpenInDefaultBrowser(String pathOrUrl)
+        {
+            // Trim any surrounding quotes and spaces.
+            pathOrUrl = pathOrUrl.Trim().Trim('"').Trim();
+            // Default protocol to "http"
+            String protocol = Uri.UriSchemeHttp;
+            // Correct the protocol to that in the actual url
+            if (Regex.IsMatch(pathOrUrl, "^[a-z]+" + Regex.Escape(Uri.SchemeDelimiter), RegexOptions.IgnoreCase))
+            {
+                Int32 schemeEnd = pathOrUrl.IndexOf(Uri.SchemeDelimiter, StringComparison.Ordinal);
+                if (schemeEnd > -1)
+                    protocol = pathOrUrl.Substring(0, schemeEnd).ToLowerInvariant();
+            }
+            // Surround with quotes
+            //   pathOrUrl = "\"" + pathOrUrl + "\"";
+            Object fetchedVal;
+            String defBrowser = null;
+            // Look up user choice translation of protocol to program id
+            using (RegistryKey userDefBrowserKey = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\Shell\Associations\UrlAssociations\" + protocol + @"\UserChoice"))
+                if (userDefBrowserKey != null && (fetchedVal = userDefBrowserKey.GetValue("Progid")) != null)
+                    // Programs are looked up the same way as protocols in the later code, so we just overwrite the protocol variable.
+                    protocol = fetchedVal as String;
+            // Look up protocol (or programId from UserChoice) in the registry, in priority order.
+            // Current User registry
+            using (RegistryKey defBrowserKey = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Classes\" + protocol + @"\shell\open\command"))
+                if (defBrowserKey != null && (fetchedVal = defBrowserKey.GetValue(null)) != null)
+                    defBrowser = fetchedVal as String;
+            // Local Machine registry
+            if (defBrowser == null)
+                using (RegistryKey defBrowserKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Classes\" + protocol + @"\shell\open\command"))
+                    if (defBrowserKey != null && (fetchedVal = defBrowserKey.GetValue(null)) != null)
+                        defBrowser = fetchedVal as String;
+            // Root registry
+            if (defBrowser == null)
+                using (RegistryKey defBrowserKey = Registry.ClassesRoot.OpenSubKey(protocol + @"\shell\open\command"))
+                    if (defBrowserKey != null && (fetchedVal = defBrowserKey.GetValue(null)) != null)
+                        defBrowser = fetchedVal as String;
+            // Nothing found. Return.
+            if (String.IsNullOrEmpty(defBrowser))
+                return false;
+            String defBrowserProcess;
+            // Parse browser parameters. This code first assembles the full command line, and then splits it into the program and its parameters.
+            Boolean hasArg = false;
+            if (defBrowser.Contains("%1"))
+            {
+                // If url in the command line is surrounded by quotes, ignore those; our url already has quotes.
+                if (defBrowser.Contains("\"%1\""))
+                    defBrowser = defBrowser.Replace("\"%1\"", pathOrUrl);
+                else
+                    defBrowser = defBrowser.Replace("%1", pathOrUrl);
+                hasArg = true;
+            }
+            Int32 spIndex;
+            if (defBrowser[0] == '"')
+                defBrowserProcess = defBrowser.Substring(0, defBrowser.IndexOf('"', 1) + 2).Trim();
+            else if ((spIndex = defBrowser.IndexOf(" ", StringComparison.Ordinal)) > -1)
+                defBrowserProcess = defBrowser.Substring(0, spIndex).Trim();
+            else
+                defBrowserProcess = defBrowser;
+
+            String defBrowserArgs = defBrowser.Substring(defBrowserProcess.Length).TrimStart();
+            // Not sure if this is possible / allowed, but better support it anyway.
+            if (!hasArg)
+            {
+                if (defBrowserArgs.Length > 0)
+                    defBrowserArgs += " ";
+                defBrowserArgs += pathOrUrl;
+            }
+            // Run the process.
+            defBrowserProcess = defBrowserProcess.Trim('"');
+            if (!File.Exists(defBrowserProcess))
+                return false;
+            ProcessStartInfo psi = new ProcessStartInfo(defBrowserProcess, defBrowserArgs);
+            psi.WorkingDirectory = Path.GetDirectoryName(defBrowserProcess);
+            Process.Start(psi);
+            return true;
+        }
+    }
+}
---
+++ MailtoHandler/clsSettings.cs
@@ -0,0 +1,330 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.IO;
+using System.Security.Cryptography;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Drawing.Printing;
+
+namespace MailtoHandler
+{
+    /// <summary>
+    /// Class: Settings
+    /// 
+    /// Class to perform all operations on XML Configuration files in a self-contained manner.
+    /// Settings may only be accessed through public methods on this class.
+    /// </summary>
+    public class Settings
+    {
+        private DataSet dsSettings;
+        private string configFilePath;
+        private bool bolChanged;
+
+        /// <summary>
+        /// Determines whether there are any unsaved changes to the settings
+        /// </summary>
+        /// <returns>true if unsaved changes exist</returns>
+        public bool Changed()
+        {
+            return bolChanged;
+        }
+
+        /// <summary>
+        /// Discard all changes since last saveXML
+        /// </summary>
+        public void Reset()
+        {
+            dsSettings.Clear();
+            dsSettings = new DataSet();
+            dsSettings.Tables.Add("Settings");
+            dsSettings.Tables["Settings"].Columns.Add("Setting");
+            dsSettings.Tables["Settings"].Columns.Add("Value");
+            dsSettings.ReadXml(configFilePath);
+        }
+
+        /// <summary>
+        /// Opens/creates a settings file
+        /// </summary>
+        /// <param name="CreateNewFile">true: create a new file, false: open existing file</param>
+        /// <param name="FileName">The path and filename of the file to open or create</param>
+        public Settings(bool CreateNewFile = false, string FileName = "config.xml", string FilePath = null)
+        {
+
+            if (FilePath != null)
+            {
+                configFilePath = Path.Combine(FilePath, FileName);
+            }
+                
+            else
+            {
+                configFilePath = Path.Combine(Environment.CurrentDirectory, FileName);
+            }
+            if (!CreateNewFile)
+            {
+                if (!File.Exists(configFilePath))
+                {
+                    throw new Exception("Cannot find configuration file");
+
+                }
+            }
+            try
+            {
+                dsSettings = new DataSet();
+                dsSettings.Tables.Add("Settings");
+                dsSettings.Tables["Settings"].Columns.Add("Setting");
+                dsSettings.Tables["Settings"].Columns.Add("Value");
+                if (File.Exists(configFilePath)) dsSettings.ReadXml(configFilePath);
+            }
+            catch
+            {
+                throw new Exception("Error reading configuration file");
+            }
+        }
+
+        /// <summary>
+        /// Read all settings from file
+        /// </summary>
+        /// <returns>Array of SettingItem[]</returns>
+        public SettingItem[] GetSettings()
+        {
+            SettingItem[] sItems = new SettingItem[dsSettings.Tables["Settings"].Rows.Count];
+            int itemCount = 0;
+            foreach (DataRow dRow in dsSettings.Tables["Settings"].Rows)
+            {
+                sItems[itemCount] = new SettingItem(dRow["Setting"].ToString(), dRow["Value"].ToString());
+                itemCount++;
+            }
+            return sItems;
+        }
+        public string Get(string Setting, string Default = null)
+        {
+            return _get(Setting, false, Default);
+        }
+        /// <summary>
+        /// Read specified setting, decrypt if requested.
+        /// </summary>
+        /// <param name="Setting">The name of the setting to return</param>
+        /// <param name="Encrypted">Whether to decrypt the value</param>
+        /// <returns>The setting's value</returns>
+        public string Get(string Setting, bool Encrypted, string Default = null)
+        {
+            return _get(Setting, Encrypted, Default);
+        }
+        protected virtual string _get(string Setting, bool Encrypted = false, string Default = null) {
+            DataRow[] dResult = dsSettings.Tables["Settings"].Select("Setting='" + Setting + "'");
+            if (dResult.Count() == 1)
+            {
+                if (Encrypted) return Decrypt(dResult[0]["Value"].ToString(), true);
+                return dResult[0]["Value"].ToString();
+            }
+            if (Default != null) return Default;
+            throw new Exception("Setting does not exist");
+        }
+
+        /// <summary>
+        /// Set the specified setting, encrypting if requested
+        /// </summary>
+        /// <param name="Setting">The name of the setting to set</param>
+        /// <param name="Value">The value of the setting</param>
+        /// <param name="Encrypted">Whether to encrypt the value</param>
+        public void Set(string Setting, string Value, bool Encrypted = false)
+        {
+            DataRow[] dResult = dsSettings.Tables["Settings"].Select("Setting='" + Setting + "'");
+            if (dResult.Count() == 1)
+            {
+                DataRow dRow = dResult[0];
+                if (Encrypted)
+                {
+                    dRow["Value"] = Encrypt(Value, true);
+                }
+                else
+                {
+                    dRow["Value"] = Value;
+                }
+                bolChanged = true;
+                return;
+            } else if(dResult.Count() == 0) {
+                DataRow dRow = dsSettings.Tables["Settings"].NewRow();
+                dRow["Setting"] = Setting;
+                if (Encrypted)
+                {
+                    dRow["Value"] = Encrypt(Value, true);
+                }
+                else
+                {
+                    dRow["Value"] = Value;
+                }
+                dsSettings.Tables["Settings"].Rows.Add(dRow);
+                bolChanged = true;
+                return;
+            }
+            throw new Exception("Error writing setting");
+        }
+
+        /// <summary>
+        /// Saves settings to XML file
+        /// </summary>
+        /// <param name="fileName">(Optional) Path and filename, will use original filename if not specified</param>
+        public void saveXML(string fileName = null) {
+            bolChanged = false;
+            
+            if (fileName != null)
+            {
+                Directory.CreateDirectory(Path.GetDirectoryName(fileName));
+                dsSettings.WriteXml(fileName);
+                return;
+            }
+            Directory.CreateDirectory(Path.GetDirectoryName(configFilePath));
+            dsSettings.WriteXml(configFilePath);
+        }
+
+        /// <summary>
+        /// Encrypts data.
+        /// </summary>
+        /// <param name="toEncrypt">The string to encrypt</param>
+        /// <param name="useHashing">Whether to use hashing</param>
+        /// <returns>Encrypted string</returns>
+        public static string Encrypt(string toEncrypt, bool useHashing)
+        {
+            byte[] keyArray;
+            byte[] toEncryptArray = UTF8Encoding.UTF8.GetBytes(toEncrypt);
+
+            
+            string key = "AK$pa$$w0rdS3Cr£t1744";
+            //System.Windows.Forms.MessageBox.Show(key);
+            //If hashing use get hashcode regards to your key
+            if (useHashing)
+            {
+                MD5CryptoServiceProvider hashmd5 = new MD5CryptoServiceProvider();
+                keyArray = hashmd5.ComputeHash(UTF8Encoding.UTF8.GetBytes(key));
+                //Always release the resources and flush data
+                // of the Cryptographic service provide. Best Practice
+
+                hashmd5.Clear();
+            }
+            else
+                keyArray = UTF8Encoding.UTF8.GetBytes(key);
+
+            TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
+            //set the secret key for the tripleDES algorithm
+            tdes.Key = keyArray;
+            //mode of operation. there are other 4 modes.
+            //We choose ECB(Electronic code Book)
+            tdes.Mode = CipherMode.ECB;
+            //padding mode(if any extra byte added)
+
+            tdes.Padding = PaddingMode.PKCS7;
+
+            ICryptoTransform cTransform = tdes.CreateEncryptor();
+            //transform the specified region of bytes array to resultArray
+            byte[] resultArray =
+              cTransform.TransformFinalBlock(toEncryptArray, 0,
+              toEncryptArray.Length);
+            //Release resources held by TripleDes Encryptor
+            tdes.Clear();
+            //Return the encrypted data into unreadable string format
+            return Convert.ToBase64String(resultArray, 0, resultArray.Length);
+        }
+        private static string Decrypt(string cipherString, bool useHashing)
+        {
+            byte[] keyArray;
+            //get the byte code of the string
+
+            byte[] toEncryptArray = Convert.FromBase64String(cipherString);
+
+            string key = "AK$pa$$w0rdS3Cr£t1744";
+
+            if (useHashing)
+            {
+                //if hashing was used get the hash code with regards to your key
+                MD5CryptoServiceProvider hashmd5 = new MD5CryptoServiceProvider();
+                keyArray = hashmd5.ComputeHash(UTF8Encoding.UTF8.GetBytes(key));
+                //release any resource held by the MD5CryptoServiceProvider
+
+                hashmd5.Clear();
+            }
+            else
+            {
+                //if hashing was not implemented get the byte code of the key
+                keyArray = UTF8Encoding.UTF8.GetBytes(key);
+            }
+
+            TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
+            //set the secret key for the tripleDES algorithm
+            tdes.Key = keyArray;
+            //mode of operation. there are other 4 modes. 
+            //We choose ECB(Electronic code Book)
+
+            tdes.Mode = CipherMode.ECB;
+            //padding mode(if any extra byte added)
+            tdes.Padding = PaddingMode.PKCS7;
+
+            ICryptoTransform cTransform = tdes.CreateDecryptor();
+            try
+            {
+                byte[] resultArray = cTransform.TransformFinalBlock(
+                                     toEncryptArray, 0, toEncryptArray.Length);
+
+                  //Release resources held by TripleDes Encryptor                
+                tdes.Clear();
+               //return the Clear decrypted TEXT
+                 return UTF8Encoding.UTF8.GetString(resultArray);
+            }
+            catch
+            {
+                tdes.Clear();
+                return "";
+            }
+        }
+    }
+
+    /// <summary>
+    /// Class to hold a setting's name and value.
+    /// </summary>
+    public class SettingItem
+    {
+        public string Setting;
+        public string Value;
+
+        public SettingItem(string sSetting, string sValue)
+        {
+            Setting = sSetting;
+            Value = sValue;
+        }
+    }
+
+    public class SerializePrinterSetting
+    {
+        public string SettingToString(PrinterSettings settings)
+        {
+            if (settings == null)
+                return null;
+
+            var bf = new BinaryFormatter();
+            using (var ms = new MemoryStream())
+            {
+                bf.Serialize(ms, settings);
+                return Convert.ToBase64String(ms.ToArray());
+            }
+        }
+
+        public PrinterSettings SettingFromString(string base64)
+        {
+            try
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                using (var ms = new MemoryStream(Convert.FromBase64String(base64)))
+                {
+                    return (PrinterSettings)bf.Deserialize(ms);
+                }
+            }
+            catch (Exception)
+            {
+                return new PrinterSettings();
+            }
+        }
+    }
+}
